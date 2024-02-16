57641be9ac580584187e7b766a056feddb1a9bb4
Larwive <larwive@gmail.com>
2024-02-16 21:34:34 +0100

    Add the List, Stack and Node implementations and tests, and add the requirement tracker.

--- src/visualgo/types/Stack.py
+++ src/visualgo/types/Stack.py
@@ -1,3 +1,5 @@
+""":demand: F1.8"""
+
 from typing import Optional, TypeVar
 from .Node import Node
 
@@ -5,34 +7,48 @@ T = TypeVar('T')
 
 
 class Stack:
+    def __init__(self):
+        self._top = Node()
+
     def push(self, value: T = None) -> None:
         """
         Pushes a value on the top of the stack.
         :param value: Any object
         :return: None
         """
-        pass
+        self._top = Node(value, self._top)
 
     def pop(self) -> Optional['T']:
         """
         Removes the top of the stack and returns it.
         :return: The top value of the stack of any type
         """
-        pass
+        if self.is_empty():
+            raise IndexError('Stack is empty')
+        val: T = self._top.content()
+        self._top = self._top.next()
+        return val
 
     def top(self) -> Optional['T']:
         """
         Returns the top of the stack.
         :return: The top value of the stack of any type
         """
-        pass
+        return self._top
 
     def is_empty(self) -> bool:
         """
         Indicates whether the stack is empty.
         :return: A boolean
         """
-        return self.__top.next() is None
+        return self._top.next() is None
 
     def __str__(self) -> str:
-        pass
+        if self._top.next() is None:
+            return "[ "
+        current_node = self._top
+        string: str = ""
+        while current_node.get_next() is not None:
+            string = str(current_node.get_value()) + " " + string
+            current_node = current_node.get_next()
+        return "[ " + string
