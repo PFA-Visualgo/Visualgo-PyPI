852a40fb6f5afac2068a29b6b29424a597771053
Fyzyy <mvidiani@enseirb.fr>
2024-02-26 22:15:38 +0100

    Refactor Queue class to use DoublyLinkedList for implementation

--- src/visualgo/types/Queue.py
+++ src/visualgo/types/Queue.py
@@ -2,16 +2,33 @@
 
 from typing import TypeVar
 
+from .DoublyLinkedList import DoublyLinkedList
+
 T = TypeVar('T')
 
 
 class Queue:
+    def __init__(self) -> None:
+        """
+        Initializes the queue.
+        """
+        self.__dll = DoublyLinkedList()
+    
+    @property
+    def head(self) -> T:
+        """
+        Returns the head of the queue.
+        :return: Object
+        """
+        return self.__dll.get(self.__dll.length() - 1)
+
     def is_empty(self) -> bool:
         """
         Checks if the queue is empty.
         :return: bool
         """
-        pass
+        return self.__dll.is_empty()
+        
 
     def enqueue(self, e: T) -> None:
         """
@@ -19,18 +36,16 @@ class Queue:
         :param e: Object
         :return: None
         """
-        pass
+        self.__dll.insert_head(e)
 
     def dequeue(self) -> T:
         """
         Removes an element from the head of the queue and returns it.
         :return: Object
         """
-        pass
-
-    def head(self) -> T:
-        """
-        Returns the head of the queue.
-        :return:
-        """
-        pass
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        else:
+            first_in = self.head
+            self.__dll.delete(self.head)
+            return first_in
