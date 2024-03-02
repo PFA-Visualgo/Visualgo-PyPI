95c83d47a942cd52b947009095f525def0dfb122
Fyzy <mvidiani@enseirb.fr>
2024-03-02 12:36:19 +0100

    Refactor TreeNode class properties and methods

--- src/visualgo/structures/TreeNode.py
+++ src/visualgo/structures/TreeNode.py
@@ -13,27 +13,27 @@ class TreeNode:
         self.__value = value
         self.__children = children
 
-
+    @property
+    def children(self) -> Set:
+        """
+        Returns the children of this TreeNode.
+        :return: List[TreeNode]
+        """
+        return self.__children
+    @property
+    def value(self) -> T:  # TODO: Changer nom dans le diagramme et le cahier des charges.
+        """
+        Returns the value of the TreeNode.
+        :return: Object
+        """
+        return self.__value
+    
     def has_child(self) -> bool:
         """
         Tells if this TreeNode has at least one child TreeNode.
         :return: bool
         """
         return not self.__children.is_empty()
-
-    def get_children(self) -> Set:
-        """
-        Returns the children of this TreeNode.
-        :return: List[TreeNode]
-        """
-        return self.__children
-
-    def get_value(self) -> T:  # TODO: Changer nom dans le diagramme et le cahier des charges.
-        """
-        Returns the value of the TreeNode.
-        :return: Object
-        """
-        return self.__value
 
     def add_child(self, tree_node: Optional['TreeNode']) -> None:
         """
