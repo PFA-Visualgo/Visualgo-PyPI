7b3171aba7c04de7fb9f8b257848f46608e71fbc
Larwive <larwive@gmail.com>
2024-03-07 12:46:02 +0100

    Move to `src`.

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@
 # in case native support for PEP 660 is not available
 from setuptools import setup, find_packages
 
+from setuptools import setup, find_packages
+
 setup(
     name='visualgo',
     version='2024.3.7',
-    packages=find_packages(include=['visualgo'])
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'}
 )
