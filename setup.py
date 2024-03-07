3e2f4c34ab6b086f359e13d761d417c833466cc0
Larwive <larwive@gmail.com>
2024-03-07 13:03:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 # in case native support for PEP 660 is not available
 from setuptools import setup, find_packages
 
-from setuptools import setup, find_packages
-
 setup(
     name='visualgo',
     version='2024.3.7',
