5dab116ac1dfb8e68a714343c24e4f8ad3379585
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-03-07 09:04:10 +0100

    added a very simple comm-api as the PoC. Needs to be better made though

--- src/visualgo/debugger/__private/comm_api/to_worker.py
+++ src/visualgo/debugger/__private/comm_api/to_worker.py
@@ -1,9 +1,37 @@
+import threading
 from typing import Callable, Any
+
+ev: threading.Event
+message_value: [str, Any]
+worker_thread: threading.Thread
+message_handler: Callable[[str, Any], None]
+
+
+def get_message_value():
+    return message_value
+
+
+def get_event() -> threading.Event:
+    return ev
+
+
+def get_message_handler() -> Callable[[str, Any], None]:
+    return message_handler
+
+
+def start_worker_task(task: Callable[[], None]) -> None:
+    global ev, worker_thread
+    ev = threading.Event()
+    worker_thread = threading.Thread(target=task)
+    worker_thread.start()
 
 
 def set_message_handler(func: Callable[[str, Any], None]):
-    pass
+    global message_handler
+    message_handler = func
 
 
 def send_message(mes_id: str, data: Any) -> None:
-    pass
+    global message_value
+    message_value = (mes_id, data)
+    ev.set()
