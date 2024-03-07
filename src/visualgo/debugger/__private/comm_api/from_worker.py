d2643c31e055e7f9ee939ee59341564a11fdc29c
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-03-07 09:04:10 +0100

    added a very simple comm-api as the PoC. Needs to be better made though

--- src/visualgo/debugger/__private/comm_api/from_worker.py
+++ src/visualgo/debugger/__private/comm_api/from_worker.py
@@ -1,9 +1,12 @@
-from typing import Any
+from typing import Any, Callable
+from .to_worker import get_message_handler, get_event, get_message_value
 
 
 def send_message(mes_id: str, data):
-    pass
+    get_message_handler()(mes_id, data)
 
 
 def wait_for_main_message() -> (str, Any):
-    pass
+    get_event().wait()
+    get_event().clear()
+    return get_message_value()
