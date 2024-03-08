4428f26d6abe53410b098091c246ff27f3871b05
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2024-03-08 19:14:17 +0100

    Replace 'pass' with raise NotImplementedError in PyDebugger

--- src/visualgo/logic/debugger/py_debugger.py
+++ src/visualgo/logic/debugger/py_debugger.py
@@ -12,23 +12,22 @@ class PyDebugger(DebuggerInterface):
     """
 
     def __init__(self, callbacks: ControllerCallbacksInterface) -> None:
-        print("PyDebugger created")
-        pass
+        raise NotImplementedError("Method not implemented yet")
 
     def set_code(self, code: str) -> None:
-        pass
+        raise NotImplementedError("Method not implemented yet")
 
     def add_breakpoint(self, line_number: int, cond: str) -> None:
-        pass
+        raise NotImplementedError("Method not implemented yet")
 
     def backward_step(self) -> None:
-        pass
+        raise NotImplementedError("Method not implemented yet")
 
     def forward_step(self) -> None:
-        pass
+        raise NotImplementedError("Method not implemented yet")
 
     def step_into(self) -> None:
-        pass
+        raise NotImplementedError("Method not implemented yet")
 
     def do_continue(self) -> None:
-        pass
+        raise NotImplementedError("Method not implemented yet")
