cd08b6817bbc14946f9ab4ca8b4d19fc6fdcd67c
Larwive <larwive@gmail.com>
2024-03-27 09:21:29 +0100

    Apply PEP8 on `tree.py` and add function version of some methods.

--- src/visualgo/structures/Tree.py
+++ src/visualgo/structures/Tree.py
@@ -3,6 +3,7 @@ from .TreeNode import TreeNode
 
 T = TypeVar('T')
 
+
 class Tree:
     def __init__(self, value: Optional[T]) -> None:
         self.__root = TreeNode(value)
@@ -10,17 +11,17 @@ class Tree:
     @property
     def root(self) -> TreeNode:
         return self.__root
-    
+
     def add_child(self, parent: TreeNode, child: TreeNode) -> None:
         if parent is None:
             self.__root = child
         else:
             parent.add_child(child)
-    
+
     def remove_child(self, parent: TreeNode, child: TreeNode) -> None:
         if parent is not None:
             parent.delete_child(child)
-    
+
     def is_empty(self) -> bool:
         return self.__root is None
 
@@ -28,7 +29,7 @@ class Tree:
         result = []
         self.__depth_first_preorder_recursive(self.__root, result)
         return result
-    
+
     def __depth_first_preorder_recursive(self, node: TreeNode, result: List[TreeNode]) -> None:
         if node is None:
             return
@@ -53,10 +54,33 @@ class Tree:
         result = []
         self.__depth_first_postorder_recursive(self.__root, result)
         return result
-    
+
     def __depth_first_postorder_recursive(self, node: TreeNode, result: List[TreeNode]) -> None:
         if node is None:
             return
         for child in node.children:
             self.__depth_first_postorder_recursive(child, result)
         result.append(node)
+
+    def __str__(self):
+        pass
+
+
+def Tree_root(tree: Tree) -> TreeNode:
+    return tree.root
+
+
+def Tree_is_empty(tree: Tree) -> bool:
+    return tree.is_empty()
+
+
+def Tree_depth_first_preorder(tree: Tree) -> List[TreeNode]:
+    return tree.depth_first_preorder()
+
+
+def Tree_depth_first_inorder(tree: Tree) -> List[TreeNode]:
+    return tree.depth_first_inorder()
+
+
+def Tree_depth_first_postorder(tree: Tree) -> List[TreeNode]:
+    return tree.depth_first_postorder()
